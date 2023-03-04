11f62c743ec30d57df29ed23ee597ac4508d8e0e
darrpama <darrpama@mail.com>
2023-03-04 15:26:10 +0300

    add tables Friends, Recommendations, XP, TimeTracking and time_tracking_state type

--- src/part1.sql
+++ src/part1.sql
@@ -12,6 +12,7 @@ CREATE TABLE Tasks
 );
 
 CREATE TYPE check_state AS ENUM ('Start', 'Success', 'Failure');
+CREATE TYPE time_tracking_state AS ENUM (1, 2);
 
 CREATE TABLE P2P
 (
@@ -44,4 +45,34 @@ CREATE TABLE TransferredPoints
     CheckingPeer VARCHAR NOT NULL,
     CheckedPeer VARCHAR NOT NULL,
     PointsAmount BIGINT NOT NULL
+);
+
+CREATE TABLE Friends
+(
+    ID BIGINT PRIMARY KEY,
+    Peer1 VARCHAR NOT NULL,
+    Peer2 VARCHAR NOT NULL
+);
+
+CREATE TABLE Recommendations
+(
+    ID BIGINT PRIMARY KEY,
+    Peer VARCHAR NOT NULL,
+    RecommendedPeer VARCHAR NOT NULL
+);
+
+CREATE TABLE XP
+(
+    ID BIGINT PRIMARY KEY,
+    "Check" BIGINT NOT NULL,
+    XPAmount BIGINT NOT NULL
+);
+
+CREATE TABLE TimeTracking
+(
+    ID BIGINT PRIMARY KEY,
+    Peer VARCHAR NOT NULL,
+    "Date" DATE NOT NULL,
+    "Time" TIME NOT NULL,
+    State time_tracking_state NOT NULL
 );
