16ddd0e156f312ed95d62c6792cd34cd28cb19c2
darrpama <darrpama@student.21-school.ru>
2023-03-10 23:33:29 +0300

    remove empty file and add testing scripts to task2

--- src/part1_task2.sql
+++ src/part1_task2.sql
@@ -18,7 +18,36 @@ $$ LANGUAGE plpgsql;
 
 ---- TESTING SCRIPTS
 TRUNCATE peers CASCADE;
-CALL import_from_csv ('peers', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/peers.csv', ',');
+CALL import_from_csv ('peers', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/01-init_peers.csv', ',');
+
+TRUNCATE tasks CASCADE;
+CALL import_from_csv ('tasks', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/02-init_tasks.csv', ',');
+
+TRUNCATE checks CASCADE;
+CALL import_from_csv ('checks', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/03-init_checks.csv', ',');
+
+TRUNCATE p2p CASCADE;
+CALL import_from_csv ('p2p', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/04-init_p2p.csv', ',');
+
+TRUNCATE verter CASCADE;
+CALL import_from_csv ('verter', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/05-init_verter.csv', ',');
+
+-- SELECT * FROM checks JOIN p2p ON checks.id = p2p.check_id WHERE state != 'failure' AND  state != 'start';
+
+TRUNCATE transferred_points CASCADE;
+CALL import_from_csv ('transferred_points', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/06-init_transferred_points.csv', ',');
+
+TRUNCATE friends CASCADE;
+CALL import_from_csv ('friends', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/07-init_friends.csv', ',');
+
+TRUNCATE recommendations CASCADE;
+CALL import_from_csv ('recommendations', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/08-init_recommendations.csv', ',');
+
+TRUNCATE xp CASCADE;
+CALL import_from_csv ('xp', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/09-init_xp.csv', ',');
+
+TRUNCATE time_tracking CASCADE;
+CALL import_from_csv ('time_tracking', '/Users/darrpama/projects/sql/SQL2_Info21_v1.0-0/src/csv/10-init_time_tracking.csv', ',');
 
 ---------------------------------------------------------------------------------------------
 -- EXPORT TO CSV
