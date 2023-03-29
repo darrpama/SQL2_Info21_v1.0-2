c7337cc4a73e0cdc78130a277f15cda2eab2f02c
myregree <kadyr.atakhanov@gmail.com>
2023-03-29 23:07:58 +0300

    update bash script; changed create enum to if not exist creating

--- src/part1.sql
+++ src/part1.sql
@@ -50,8 +50,13 @@ CREATE TABLE IF NOT EXISTS checks (
 
 -- ENUMS
 ---------------------------------------------------------------
-CREATE TYPE check_state
-    AS ENUM ('start', 'success', 'failure');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'check_state') THEN
+        CREATE TYPE check_state AS ENUM ('start', 'success', 'failure');
+    END IF;
+    --more types here...
+END$$;
 
 
 -- P2P table
@@ -178,39 +183,39 @@ $$ LANGUAGE plpgsql;
 
 ---- TESTING SCRIPTS
 TRUNCATE peers CASCADE;
-CALL import_from_csv ('peers', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/01-init_peers.csv', ',');
+CALL import_from_csv ('peers', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//01-init_peers.csv', ',');
 
 TRUNCATE tasks CASCADE;
-CALL import_from_csv ('tasks', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/02-init_tasks.csv', ',');
+CALL import_from_csv ('tasks', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//02-init_tasks.csv', ',');
 
 TRUNCATE checks CASCADE;
-CALL import_from_csv ('checks', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/03-init_checks.csv', ',');
+CALL import_from_csv ('checks', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//03-init_checks.csv', ',');
 
 TRUNCATE p2p CASCADE;
-CALL import_from_csv ('p2p', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/04-init_p2p.csv', ',');
+CALL import_from_csv ('p2p', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//04-init_p2p.csv', ',');
 
 TRUNCATE verter CASCADE;
-CALL import_from_csv ('verter', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/05-init_verter.csv', ',');
+CALL import_from_csv ('verter', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//05-init_verter.csv', ',');
 
 -- SELECT checking_peer, peer FROM checks JOIN p2p ON checks.id = p2p.check_id WHERE state != 'start' and state != 'failure' ORDER BY checking_peer, peer;
 -- SELECT checking_peer, peer FROM checks JOIN p2p ON checks.id = p2p.check_id WHERE state != 'start' ORDER BY checking_peer, peer;
 
 TRUNCATE transferred_points CASCADE;
-CALL import_from_csv ('transferred_points', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/06-init_transferred_points.csv', ',');
+CALL import_from_csv ('transferred_points', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//06-init_transferred_points.csv', ',');
 
 TRUNCATE friends CASCADE;
-CALL import_from_csv ('friends', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/07-init_friends.csv', ',');
+CALL import_from_csv ('friends', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//07-init_friends.csv', ',');
 
 TRUNCATE recommendations CASCADE;
-CALL import_from_csv ('recommendations', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/08-init_recommendations.csv', ',');
+CALL import_from_csv ('recommendations', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//08-init_recommendations.csv', ',');
 
 TRUNCATE xp CASCADE;
-CALL import_from_csv ('xp', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/09-init_xp.csv', ',');
+CALL import_from_csv ('xp', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//09-init_xp.csv', ',');
 
 -- SELECT check_id, max_xp FROM checks JOIN verter v on checks.id = v.check_id join tasks t on checks.task = t.title WHERE state != 'start' and state != 'failure';
 
 TRUNCATE time_tracking CASCADE;
-CALL import_from_csv ('time_tracking', '/Users/myregree/projects/sql/SQL2_Info21_v1.0-0/src/csv/10-init_time_tracking.csv', ',');
+CALL import_from_csv ('time_tracking', '/Users/myregree/Desktop/projects/SQL2_Info21_v1.0-0/src/csv//10-init_time_tracking.csv', ',');
 
 ---------------------------------------------------------------------------------------------
 -- EXPORT TO CSV
