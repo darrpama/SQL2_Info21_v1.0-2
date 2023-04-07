20d2c4811b1c67630b49e51525e0780a63025d83
darrpama <darrpama@student.21-school.ru>
2023-04-07 22:16:35 +0300

    start rewriting task1 and task3

--- src/part2_task1.sql
+++ src/part2_task1.sql
@@ -2,13 +2,27 @@ CREATE OR REPLACE PROCEDURE pr_add_p2p_check(new_checked_peer text, new_checking
 AS
 $$
 DECLARE
-    new_check_id BIGSERIAL := 0;
+    new_check_id BIGINT := 0;
+    new_p2p_id BIGINT := 0;
 BEGIN
     IF new_state = 'start'
     THEN
-        new_check_id = (SELECT max(id) FROM checks) + 1;
-        INSERT INTO checks (id, peer, task, check_date)
-        VALUES (new_check_id, new_checked_peer, new_task_title, (SELECT CURRENT_DATE));
+        IF ((SELECT count(*) FROM p2p
+            JOIN checks ON p2p.check_id = checks.id
+            WHERE p2p.checking_peer = new_checking_peer
+            AND checks.peer = new_checked_peer
+            AND checks.task = new_task_title) = TRUE)
+        THEN
+            RAISE EXCEPTION 'Невозможно добавить проверку для данных пиров, так как у них есть незавершённая проверка';
+        ELSE
+            SELECT @new_check_id = (SELECT max(id) FROM checks) + 1;
+            INSERT INTO checks (id, peer, task, check_date)
+            VALUES (new_check_id, new_checked_peer, new_task_title, now());
+
+            SELECT @new_p2p_id = (SELECT max(id) FROM p2p) + 1;
+            INSERT INTO p2p (id, check_id, checking_peer, state, check_time)
+            VALUES (new_p2p_id, new_check_id - 1, checking_peer, state, check_time);
+        END IF;
     ELSE
         new_check_id = (SELECT checks.id FROM p2p
                     INNER JOIN checks ON checks.id = p2p.check_id
@@ -16,8 +30,10 @@ BEGIN
                     AND p2p.checking_peer = $2
                     AND task = $3
                     ORDER BY checks.id DESC LIMIT 1);
+        INSERT INTO p2p (check_id, checking_peer, state, check_time)
+        VALUES (new_check_id, new_checking_peer, new_state, new_check_time);
     END IF;
-    INSERT INTO p2p (check_id, checking_peer, state, check_time)
-    VALUES (new_check_id, new_checking_peer, new_state, new_check_time);
 END;
 $$ LANGUAGE plpgsql;
+
+CALL pr_add_p2p_check('darrpama', 'myregree', 'C7_SmartCalc_v1.0', 'start', '15:30:01')
--- src/part2_task3.sql
+++ src/part2_task3.sql
@@ -3,26 +3,28 @@ RETURNS TRIGGER AS
 $$
 BEGIN
     IF NEW.state = 'start' THEN
-    UPDATE transferred_points
-    SET points_amount = transferred_points.points_amount + 1,
-    FROM (
+    WITH foo AS (
         SELECT checking_peer, peer FROM p2p
         JOIN checks ON p2p.check_id = checks.id
-        WHERE state = 'start' AND NEW."check_id" = checks.id
+        WHERE state = 'start' AND checks.id = 4
         )
-    WHERE ...
+    UPDATE transferred_points
+    SET points_amount = transferred_points.points_amount + 1
+    FROM foo
+    WHERE foo.peer = transferred_points.checked_peer AND
+          NEW.checking_peer = transferred_points.checking_peer;
     ELSE
         RETURN NULL;
     END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT checking_peer, peer FROM p2p
-JOIN checks ON p2p.check_id = checks.id
-WHERE state = 'start' AND checks.id = 4;
 
 
-CREATE TRIGGER  trg_transfer_p2p_point
+CREATE TRIGGER trg_transfer_p2p_point
     AFTER INSERT ON p2p
     FOR EACH ROW
 EXECUTE FUNCTION fnc_transfer_p2p_point();
+
+
+INSERT INTO checks VALUES ('', );
