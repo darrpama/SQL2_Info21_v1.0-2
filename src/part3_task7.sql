b75183816876f9e62ee5ad5157e1c519c2a8fa43
dedelmir <dedelmir@student.21-school.ru>
2023-04-13 02:10:59 +0300

    task 7 / issue #16

--- src/part3_task7.sql
+++ src/part3_task7.sql
@@ -12,7 +12,9 @@ BEGIN
             (SELECT DISTINCT peer, task,
                  FIRST_VALUE(check_date) OVER (PARTITION BY peer, task ORDER BY check_date) as date
              FROM (SELECT * FROM checks WHERE task SIMILAR TO CONCAT(block, '[0-9]%')) as c
-                   JOIN p2p p ON c.id = p.check_id WHERE state = 'success')
+                   LEFT JOIN p2p p ON c.id = p.check_id
+                   LEFT JOIN verter v ON c.id = v.check_id
+            WHERE p.state = 'success' AND (v.state IS NULL OR v.state = 'success'))
         SELECT peer, MAX(date) as date FROM successful_checks GROUP BY peer
         HAVING COUNT(task) = (SELECT COUNT(title) FROM tasks WHERE title SIMILAR TO CONCAT(block, '[0-9]%'));
 END;
