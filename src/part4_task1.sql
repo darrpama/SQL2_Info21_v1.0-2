227aae8c376cdf61cd7ebb285822e54a84ef2782
dedelmir <dedelmir@student.21-school.ru>
2023-04-16 00:35:50 +0300

    issue #29 add table settings

--- src/part4_task1.sql
+++ src/part4_task1.sql
@@ -3,10 +3,14 @@ AS $$
 DECLARE
     rec record;
 BEGIN
-    FOR rec IN (SELECT * FROM information_schema.tables WHERE table_name SIMILAR TO 'tablename%')
+    FOR rec IN (SELECT * FROM information_schema.tables
+    WHERE table_catalog = (SELECT CURRENT_DATABASE())
+        AND table_schema = (SELECT CURRENT_SCHEMA())
+        AND table_type = 'BASE TABLE'
+        AND table_name SIMILAR TO 'tablename%')
     LOOP
-        EXECUTE 'DROP TABLE ' ||  quote_ident(rec.table_name);
-        RAISE INFO 'Dropped table: %', quote_ident(rec.table_name);
+        EXECUTE 'DROP TABLE ' || QUOTE_IDENT(rec.table_name);
+        RAISE INFO 'Dropped table: %', QUOTE_IDENT(rec.table_name);
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
